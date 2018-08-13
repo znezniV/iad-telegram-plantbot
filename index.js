ec15820e7c0b3ab5e94f6127f9faef73c2373850
znezniV <vleutenegger@gmail.com>
2018-08-13 15:20:54 +0200

    play with sample messages

--- index.js
+++ index.js
@@ -9,3 +9,28 @@ const token = loadedToken.telegramToken;
 
 // create new bot
 const bot = new TelegramBot(token, { polling: true });
+
+// test
+bot.on('message', (msg) => {
+
+    var hi = "hi";
+    if (msg.text.toString().toLowerCase().indexOf(hi) === 0) {
+        bot.sendMessage(msg.chat.id, "Hello dear user");
+    }
+
+    var bye = "bye";
+    if (msg.text.toString().toLowerCase().includes(bye)) {
+        bot.sendMessage(msg.chat.id, "Hope to see you around again , Bye");
+    }
+
+});
+
+bot.onText(/\/start/, (msg) => {
+
+    bot.sendMessage(msg.chat.id, "Hi plant lover " + msg.from.first_name, {
+        "reply_markup": {
+            "keyboard": [["Sample text", "Second sample"], ["Keyboard"], ["I'm robot"]]
+        }
+    });
+
+});

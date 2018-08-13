2d760f5f9e0c011d934009e72b360207a8b1616a
znezniV <vleutenegger@gmail.com>
2018-08-13 16:15:05 +0200

    add dummy plant data

---
+++ plants_config.js
@@ -0,0 +1,18 @@
+var date = Date.now();
+
+module.exports = { 
+    "features": [
+        {
+            "name": "Plant 1",
+            "daysWaterFreq": 7,
+            "lastWatered": date,
+            fine: true
+        },
+        {
+            "name": "Plant 2",
+            "daysWaterFreq": 7,
+            "lastWatered": date,
+            fine: true
+        }
+    ]
+};

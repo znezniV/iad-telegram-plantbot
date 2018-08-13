dbf03a0bc95c5eac0a87e0b4bbaf44338cbe9a30
znezniV <vleutenegger@gmail.com>
2018-08-13 15:20:35 +0200

    load and assign token from file

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // load node module
 const TelegramBot = require('node-telegram-bot-api');
+
+// load token
+const loadedToken = require('./token');
+
+// assign token
+const token = loadedToken.telegramToken;

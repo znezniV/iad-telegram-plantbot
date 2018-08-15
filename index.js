1eb1acbc2353491fc19a0a03ca9506c360e8ebf6
znezniV <vleutenegger@gmail.com>
2018-08-15 14:53:57 +0200

    removed unused function

--- index.js
+++ index.js
@@ -27,19 +27,8 @@ const bot = new TelegramBot(token, { polling: true });
 // any message
 bot.on('message', (msg) => {
 
-    var hi = "hi";
-    if (msg.text.toString().toLowerCase().indexOf(hi) === 0) {
 
-        bot.sendMessage(msg.chat.id, "Hello dear user");
 
-    }
-
-    var bye = "bye";
-    if (msg.text.toString().toLowerCase().includes(bye)) {
-
-        bot.sendMessage(msg.chat.id, "Hope to see you around again , Bye");
-
-    }
 });
 
 // start message
